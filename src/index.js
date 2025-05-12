1201595a3699ad95d5a2f1e1aba56f8536b94104
tnhi1603 <21521236@gm.uit.edu.vn>
2025-05-12 20:24:32 +0700

    add

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import "./index.css";
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/plant-shop">
   <Provider store={store}>
     <Router>
       <App />
     </Router>
   </Provider>
-  </BrowserRouter>
 );
